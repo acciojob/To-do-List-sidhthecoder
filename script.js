3654d296634ed694b15c4394fd80da5a92635ba3
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-02-15 18:46:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+var todo=document.getElementById("newTodoInput");
+var btn= document.getElementById("addTodoBtn");
+var ol=document.getElementById("todoList");
 
+function addTodo(){
+	if(todo.value){
+     var list=document.createElement("li");
+		list.textContent=todo.value;
+	     ol.appendChild(list);
+		todo.value="";
+	}
+}
+btn.addEventListener('click',addTodo);
